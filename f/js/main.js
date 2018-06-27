1b72270634e9322fb79d1c22188faf8571e6e491
BerTepesh <Ber.Tepesh@gmail.com>
2018-06-27 17:12:34 +0300

    Страница товара

--- f/js/main.js
+++ f/js/main.js
@@ -12,4 +12,10 @@ $(document).ready(function() {
     var stHeight = $(this).find('.slick-track').height();
     $(this).find('.slick-slide').css('height',stHeight + 'px' );
   });
+  $(".preface__nav a").click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 400);
+    return false;
+  });
 });
